ef57b40e5f9ade7e085d3fb836d256d1c095a2f8
Thiago Silva Farias <thiago_protect@hotmail.com>
2024-04-07 15:52:38 -0300

    added new folter 'Data Insights': Em uma empresa a área de Data Insights analisa dados para fornecer insights acionáveis que impulsionam a tomada de decisões estratégicas e o crescimento do negócio.

---
+++ 7 - Data Insights (em desenvolvimento)/queries_data_insights.sql
@@ -0,0 +1,20 @@
+/* 1 - Market Basket Analysis (Análise de Cesta de Compras): Identifica padrões de compra e associações entre os itens comprados pelos clientes. */
+
+/* 2 - Taxa de Churn: Mede a proporção de clientes que cancelam ou não renovam seus serviços em um determinado período de tempo. */
+
+/* 3 - Lifetime Value (LTV) dos Clientes: Representa o valor líquido que um cliente contribui para a empresa durante todo o seu relacionamento com ela. */
+
+/* 4 - Custo de Aquisição de Clientes (CAC): Mostra o custo médio de adquirir um novo cliente para a empresa. */
+
+/* 5 - Lifetime Value (LTV) dos Clientes: Representa o valor líquido que um cliente contribui para a empresa durante todo o seu relacionamento com ela. */
+
+/* 6 - Taxa de Conversão: Indica a proporção de usuários que realizam uma ação desejada, como fazer um pedido, em relação ao número total de usuários. */
+
+/* 7 - Taxa de Engajamento do Usuário: Avalia o nível de interação dos usuários com a plataforma ou aplicativo. */
+
+/* 8 - Análise de Cohort: Agrupa os clientes com base em características semelhantes para analisar seu comportamento ao longo do tempo. */
+
+/* 9 - Tempo Médio de Resposta (MTTR): Mede o tempo médio necessário para resolver problemas ou responder a solicitações dos clientes. */
+
+/* 10 - Taxa de Retenção de Usuários: Essa métrica mede a capacidade da empresa de manter seus clientes ao longo do tempo. */
+
